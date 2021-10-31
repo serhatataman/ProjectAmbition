31a86d9eb279f6ba995a10a073e7eb0aef085b54
Serhat <serhatataman13@hotmail.com>
2021-10-31 18:52:46 +0100

    discriminator model is created

---
+++ Discriminator/discriminator.py
@@ -0,0 +1,59 @@
+from keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model, load_model
+from keras.optimizers import Adam
+import numpy as np
+from PIL import Image
+import os
+
+
+def discriminator(image_shape):
+    # We are initializing a Sequential model from keras which helps us in creating linear stacks of layers
+    model = Sequential()
+
+    # Our first layer is a convolutional layer of 32 shape having
+    # kernel_size of 3 and our stride value is 2 with padding same. Since it is a first layer it holds input_shape.
+    # To understand what is going on here, you can refer to keras official documentation page
+    # But in simple language, here we are defining a convolutional layer which has
+    # a filter of size 3X3 and that filter strides over our image data.
+    # We have padding of same which means, no additional paddings are added. It remains the same as the original.
+    model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=image_shape, padding="same"))
+    # We are adding a LeakyRelu layer which is an activation function
+    model.add(LeakyReLU(alpha=0.2))
+    # Similarly in other block of layers are added in a sequential model
+    # with some dropouts and batch normalization to prevent overfitting
+    model.add(Dropout(0.25))
+    
+    model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+    model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    
+    model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    
+    model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    
+    model.add(Dropout(0.25))
+    model.add(Conv2D(512, kernel_size=3, strides=1, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    
+    model.add(Dropout(0.25))
+    # The last layer of our model is a Fully connected layer with an activation function sigmoid
+    model.add(Flatten())
+    # Since our discriminator’s job is to classify whether the given image is fake or not,
+    # it is a binary classification task and sigmoid is an activation
+    # that squeezes every value to values between 0 and 1
+    model.add(Dense(1, activation='sigmoid'))
+    
+    input_image = Input(shape=image_shape)
+    validity = model(input_image)
+
+    return Model(input_image, validity)
--- startup.py
+++ startup.py
@@ -1,1 +1,38 @@
-print("I have just started")
+from keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model, load_model
+from keras.optimizers import Adam
+import numpy as np
+from PIL import Image
+import os
+
+# Preview image Frame
+PREVIEW_ROWS = 4
+PREVIEW_COLS = 7
+PREVIEW_MARGIN = 4
+SAVE_FREQ = 100
+
+# Size vector to generate images from.
+# NOISE_SIZE here is a latent dimension size to generate our images.
+NOISE_SIZE = 100
+
+# Configurations - Note: Images should always be of square sizeo
+
+# EPOCHS is a number of iterations: it defines how many times we want to iterate over our training images
+EPOCHS = 10000
+# BATCH_SIZE is a number of images to feed in every iteration.
+BATCH_SIZE = 32
+GENERATE_RES = 3
+# IMAGE_SIZE is our image size which we resized earlier to 128X128
+IMAGE_SIZE = 128  # rows/cols
+# IMAGE_CHANNELS is a number of channel in our images; which is 3
+IMAGE_CHANNELS = 3
+
+# Loading resized images
+resized_images_path = os.path.join("DataWashing/ResizedImages/", "resized_images_data.npy")
+if not os.path.exists(resized_images_path):
+    raise FileNotFoundError("No resized images are found at directory: " + resized_images_path)
+
+training_data = np.load(resized_images_path)
+
